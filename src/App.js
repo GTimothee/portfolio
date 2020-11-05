df6ffe15c735767d2784e5af54b230498c4b080b
GTimothee <39728445+GTimothee@users.noreply.github.com>
2020-11-05 16:14:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome!
-          The website is coming soon...
+          Portfolio de Timothée Guédon.
+          Site web en construction...
         </p>
       </header>
     </div>
